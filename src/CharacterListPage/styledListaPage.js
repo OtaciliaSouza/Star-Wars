a306ce5225b327089aed8d515782b60e640023ce
Otacilia <otaciliatimoteo@hotmail.com>
2022-05-09 15:03:12 -0300

    styled

--- src/CharacterListPage/styledListaPage.js
+++ src/CharacterListPage/styledListaPage.js
@@ -4,10 +4,36 @@
 
 import styled from "styled-components"
 
+export const FirstContainer = styled.div`
+align-items: center;
+ padding: 4rem 2rem 2rem;
+ background: linear-gradient(#663300, #996633, #999966);
+ box-shadow: inset 0 0 1em black,0 0 1em black ;
+
+ img {
+  
+    margin-left:40%;
+    width: 20%;
+    height:100%;
+   
+  }
+
+
+
+`;
+export const FirstTitle = styled.h1`
+text-align: center;
+color: azure;
+`;
+
+
 export const CharacterCard = styled.div`
     border-radius: 6px;
     background-color: #996633;
+    box-shadow: inset 0 0 1em #663300,0 0 1em #996633;
 
+text-align: center;
+font-size: larger;
 padding: 10px;
 margin: 10px;
 width:  300px;
@@ -17,3 +43,10 @@ width:  300px;
 }
 
 `
+export const caracter = styled.h1`
+   margin-left:35%;
+   text-align:center;
+    width: 30%;
+    height:100%;
+
+`;
